412dedc4227c3f022a497cfd48c740b119689ac7
Zane Starr <zcstarr@gmail.com>
2021-12-29 19:56:55 -0800

    chore: fix initial build script to build generatd client as well

--- packages/simple-nft/publish.sh
+++ packages/simple-nft/publish.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-npm run build
+npm run build:all
 echo "//registry.npmjs.org/:_authToken=$NPM_TOKEN" > generated-client/typescript/.npmrc
 cd  generated-client/custom/typescript && npm publish --access=public
